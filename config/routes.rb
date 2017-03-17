20057043e9fb98c251fd19f1977924100ac2be57
Rob Head <robert.head@gmail.com>
2017-03-17 10:49:27 -0700

    Create basic layout and top navigation bar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/show'
+  get 'abouts/show'
+
+  resource :home, only: [:show]
+  resource :about, only: [:show]
 
   root to: 'homes#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
